95ea1bbd8dc6fdd33e640c14b1e4f13a4e0b2997
MelvinDinh3302 <trongthanghsp@gmail.com>
2025-05-12 15:33:59 -0400

    add a basic picker

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import { Button, Image, View, StyleSheet, Dimensions } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { Picker } from '@react-native-picker/picker';
 
 const { width, height } = Dimensions.get('window');
 
 export default function ImagePickerExample() {
   const [image, setImage] = useState<string | null>(null);
+  const [type, setType] = useState('d');
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -22,9 +24,21 @@ export default function ImagePickerExample() {
 
   return (
     <View style={styles.container}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={styles.image} />}
+      <Button title="Pick an image" onPress={pickImage} />
+      <View style={{ marginVertical: 10 }}>
+        {image && <Image source={{ uri: image }} style={styles.image} />}
+      </View>
+      <Picker
+        selectedValue={type}
+        onValueChange={(itemValue) => setType(itemValue)}
+        style={styles.picker}
+      >
+        <Picker.Item label="Deuteranopia" value="d" />
+        <Picker.Item label="Protanopia" value="p" />
+        <Picker.Item label="Tritanopia" value="t" />
+      </Picker>
     </View>
+
   );
 }
 
@@ -39,4 +53,8 @@ const styles = StyleSheet.create({
     height: height * 0.7,
     resizeMode: 'contain', 
   },
+  picker: {
+    height: 50, 
+    width: width * 0.5,
+  }
 });
